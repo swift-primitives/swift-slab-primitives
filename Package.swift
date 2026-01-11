90b29ad95e1da1c128caa68831f63e6d60c0c4f9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-11 19:05:14 +0100

    Rename test dependency to swift-test-primitives

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../swift-standard-library-extensions"),
-        .package(path: "../swift-test-support-primitives"),
+        .package(path: "../swift-test-primitives"),
     ],
     targets: [
         .target(
@@ -32,7 +32,7 @@ let package = Package(
             name: "Storage Primitives Tests",
             dependencies: [
                 "Storage Primitives",
-                .product(name: "Test Support Primitives", package: "swift-test-support-primitives"),
+                .product(name: "Test Primitives", package: "swift-test-primitives"),
             ]
         ),
     ],
