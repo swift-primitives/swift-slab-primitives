a29f58977dd2902e65b0ddfc59cc7e744a804e43
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-15 11:47:26 +0100

    Use local path dependencies
    Convert remote URL dependencies to local path dependencies for
    development workflow.

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", from: "0.0.1"),
-        .package(url: "https://github.com/swift-primitives/swift-test-primitives.git", from: "0.0.1"),
+        .package(path: "../swift-standard-library-extensions"),
+        .package(path: "../swift-test-primitives"),
     ],
     targets: [
         .target(
