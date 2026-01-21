1279f74d475b8c37429c238c2b839dcd74a4a7df
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-21 10:23:44 +0100

    Add Index, Bit, Reference, and Collection Primitives dependencies
    Slab allocators use indexed slots, allocation bitmaps, reference
    management, and are enumerable.

--- Package.swift
+++ Package.swift
@@ -18,13 +18,21 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(path: "../swift-standard-library-extensions")
+        .package(path: "../swift-standard-library-extensions"),
+        .package(path: "../swift-index-primitives"),
+        .package(path: "../swift-bit-primitives"),
+        .package(path: "../swift-reference-primitives"),
+        .package(path: "../swift-collection-primitives"),
     ],
     targets: [
         .target(
             name: "Slab Primitives",
             dependencies: [
-                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions")
+                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
+                .product(name: "Index Primitives", package: "swift-index-primitives"),
+                .product(name: "Bit Primitives", package: "swift-bit-primitives"),
+                .product(name: "Reference Primitives", package: "swift-reference-primitives"),
+                .product(name: "Collection Primitives", package: "swift-collection-primitives"),
             ]
         )
     ],
