d368d302cac715b3b77acb74ea78ee81130df75e
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-21 10:27:17 +0100

    Add exports.swift with @_exported re-exports
    Re-export all semantic dependencies for consumers.

---
+++ Sources/Slab Primitives/exports.swift
@@ -0,0 +1,8 @@
+// exports.swift
+// Re-export dependencies for consumers.
+
+@_exported import Standard_Library_Extensions
+@_exported import Index_Primitives
+@_exported import Bit_Primitives
+@_exported import Reference_Primitives
+@_exported import Collection_Primitives
